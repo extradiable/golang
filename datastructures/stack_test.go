07fd8cc65b747edaeef90141fc5f5838a1badadd
Victor Valle <extradiable@gmail.com>
2023-03-06 16:41:19 -0600

    tests for dynamic stacks were updated

--- datastructures/stack_test.go
+++ datastructures/stack_test.go
@@ -5,68 +5,40 @@ import (
 	"testing"
 )
 
-// TestNewStack verifies that new Stacks are empty
-func TestNewStack(t *testing.T) {
+// TestEmptyStack:
+// Verify that new stacks are empty
+func TestEmptyStack(t *testing.T) {
 	stack := CreateDStack()
 	var boolExpected bool = true
 	var boolResult bool = stack.Empty()
 	if boolResult != boolExpected {
-		t.Fatalf("[step 1] stack.Empty(): expected value %s, got: %s", strconv.FormatBool(boolExpected), strconv.FormatBool(boolResult))
-	}
-	var intExpected int = 0
-	var intResult int = stack.Size()
-	if intResult != intExpected {
-		t.Fatalf("[step 2] stack.Size(): expected value %d, got %d.", intExpected, intResult)
-	}
-	_, err := stack.Pop()
-	if err == nil {
-		t.Fatal("[step 3] stack.Pop(): expected error: stack is empty, got: nil")
-	}
-	stack.Push(1)
-	boolExpected = false
-	boolResult = stack.Empty()
-	if boolResult != boolExpected {
-		t.Fatalf("[step 4] stack.Empty(): expected value %s, got: %s", strconv.FormatBool(boolExpected), strconv.FormatBool(boolResult))
-	}
-	intExpected = 1
-	intResult = stack.Size()
-	if intResult != intExpected {
-		t.Fatalf("[step 5] stack.Size(): expected value %d, got %d.", intExpected, intResult)
-	}
-	stack.Pop()
-	boolExpected = true
-	boolResult = stack.Empty()
-	if boolResult != boolExpected {
-		t.Fatalf("[step 6] stack.Empty(): expected value %s, got: %s", strconv.FormatBool(boolExpected), strconv.FormatBool(boolResult))
-	}
-	intExpected = 0
-	intResult = stack.Size()
-	if intResult != intExpected {
-		t.Fatalf("[step 7] stack.Size(): expected value %d, got %d.", intExpected, intResult)
+		t.Fatalf("stack.Empty(): expected value %s, got: %s", strconv.FormatBool(boolExpected), strconv.FormatBool(boolResult))
 	}
 }
 
-func TestMultiplePush(t *testing.T) {
+// TestPushAndPop
+// test a push followed by a pop
+func TestPushAndPop(t *testing.T) {
 	stack := CreateDStack()
-	var pushes int = 17
-	var pops int = 12
-	for i := 1; i <= pushes; i++ {
-		stack.Push(i * 10)
+	expectedValue := "hello"
+	stack.Push(expectedValue)
+	result, _ := stack.Pop()
+	if expectedValue != result.(string) {
+		t.Fatalf("stack.Pop(): expected value %s, got %v.", expectedValue, result)
 	}
-	for i := 1; i <= pops; i++ {
+}
+
+func TestUnderflow(t *testing.T) {
+	stack := CreateDStack()
+	iterations := 1024
+	for i := 0; i < iterations; i++ {
+		stack.Push(i)
+	}
+	for i := 0; i < iterations; i++ {
 		stack.Pop()
 	}
-	intExpected := pushes - pops
-	intResult := stack.Size()
-	if intResult != intExpected {
-		t.Fatalf("[step 1] stack.Size(): expected value %d, got %d.", intExpected, intResult)
-	}
-	pushes = 120
-	for i := 1; i <= pushes; i++ {
-		stack.Push(i * 10)
-	}
-	pops = 90
-	for i := 1; i <= pops; i++ {
-		stack.Pop()
+	_, err := stack.Pop()
+	if err != ErrStackUnderflow {
+		t.Fatalf("stack.Pop(): expected '%v' error got '%v'.", ErrStackUnderflow, err)
 	}
 }
